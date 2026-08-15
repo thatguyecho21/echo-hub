d28a4354cd1585eb772b960e56decc8f5edec030
thatguyecho21 <boogies.oracles7n@icloud.com>
2026-08-15 17:54:15 -0400

    Create RM.lua

---
+++ RM.lua
@@ -0,0 +1,25 @@
+local Rayfield = loadstring(game:HttpGet("https://sirius.menu/gen2"))()
+
+local window = Rayfield:CreateWindow({
+    name = "Example Hub",
+    subtitle = "Rayfield Gen2",
+    theme = "frost",
+})
+
+local n1 = window:CreateTab({ name = "Night 1", icon = "house" })
+local n2 = window:CreateTab({ name = "Night 2", icon = "factory" })
+local n3 = window:CreateTab({ name = "Night 3", icon = "shelving-unit" })
+
+n1:CreateButton({
+    name = "Say hello",
+    callback = function()
+        window:Notify({ title = "Hello", content = "Your first element works." })
+    end,
+})
+
+n1:CreateToggle({
+    name = "Auto Sprint",
+    callback = function(value)
+        print("Auto Sprint:", value)
+    end,
+})
