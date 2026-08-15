69ef6773a8bc22f9c4a452ae33c3dfdc7c6ef9c5
thatguyecho21 <boogies.oracles7n@icloud.com>
2026-08-15 17:56:15 -0400

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,16 @@
+local Rayfield = loadstring(game:HttpGet("https://sirius.menu/gen2"))()
+
+local window = Rayfield:CreateWindow({
+    name = "ECHO HUB | LOADER",
+    subtitle = "Many scripts to choose from.",
+    theme = "frost",
+})
+
+local loader = window:CreateTab({ name = "Loader", icon = "settings" })
+
+loader:CreateButton({
+    name = "Residence Massacre",
+    callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/thatguyecho21/echo-hub/refs/heads/main/RM.lua"))()
+    end,
+})
