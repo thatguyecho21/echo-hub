8309424bb3609ad4ce4dc1de1382059af4c414f4
thatguyecho21 <boogies.oracles7n@icloud.com>
2026-08-16 14:49:55 -0400

    Update RM.lua

--- Games/RM.lua
+++ Games/RM.lua
@@ -5,24 +5,15 @@ end
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/gen2"))()
 
 local window = Rayfield:CreateWindow({
-    name = "ECHO HUB",
-    subtitle = "RESIDENCE MASSACRE SCRIPT",
+    name = "ECHO HUB | LOADER",
+    subtitle = "Click a button and it will launch the script.",
 })
 
-local n1 = window:CreateTab({ name = "Night 1", icon = "93364949241311" })
-local n2 = window:CreateTab({ name = "Night 2", icon = "93364949241311" })
-local n3 = window:CreateTab({ name = "Night 3", icon = "93364949241311-unit" })
+local loader = window:CreateTab({ name = "Loader", icon = nil })
 
-n1:CreateButton({
-    name = "Say hello",
+loader:CreateButton({
+    name = "Residence Massacre",
     callback = function()
-        window:Notify({ title = "Hello", content = "Your first element works." })
-    end,
-})
-
-n1:CreateToggle({
-    name = "Auto Sprint",
-    callback = function(value)
-        print("Auto Sprint:", value)
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/thatguyecho21/echo-hub/refs/heads/main/Games/RM.lua"))()
     end,
 })
