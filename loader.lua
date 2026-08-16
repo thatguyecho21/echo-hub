9929e6304ac401ba5eb906c107124fc9653b2473
thatguyecho21 <boogies.oracles7n@icloud.com>
2026-08-16 13:35:26 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,7 +3,6 @@ local Rayfield = loadstring(game:HttpGet("https://sirius.menu/gen2"))()
 local window = Rayfield:CreateWindow({
     name = "ECHO HUB | LOADER",
     subtitle = "Click a button and it will launch the script.",
-    theme = "cobalt",
 })
 
 local loader = window:CreateTab({ name = "Loader", icon = "settings" })
